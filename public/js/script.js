5435ad7d206dee9cd150229cc8a2e7b9d099e2fd
Schleimfresse <87274892+Schleimfresse@users.noreply.github.com>
2022-10-15 12:07:55 +0200

    2.0.0 | stable

--- public/js/script.js
+++ public/js/script.js
@@ -1,22 +1,34 @@
 const td = document.getElementById("td");
 const tm = document.getElementById("tm");
-const td_additional = document.getElementById("td-addional-i")
+const tm_heading = document.getElementById("tm-heading");
+const td_additional = document.getElementById("td-addional-i");
+const tm_additional = document.getElementById("tm-addional-i");
 const initialise = () => {
-	let lessons = data[0].data;
-	let additional = data[0].additional;
-	lessons.concat(data[1].data);
-	additional.concat(data[1].additional);
+	const lessons = [data[0].data, data[1].data];
+	const additional = [data[0].additional, data[1].additional];
+	tm_heading.textContent += `, ${data[1].date}`;
 	lessons.forEach((e) => {
+		e.forEach((e) => {
+			const div = document.createElement("div");
+			div.innerHTML = e.info;
+			if (e.type === "today") {
+				td.append(div);
+			}
+			if (e.type === "tomorrow") {
+				tm.append(div);
+			}
+		});
+	});
+	additional.forEach((e) => {
 		const div = document.createElement("div");
 		div.innerHTML = e.info;
 		if (e.type === "today") {
-			td.append(div);
+			td_additional.append(div);
 		}
 		if (e.type === "tomorrow") {
-			tm.append(div);
+			tm_additional.append(div);
 		}
 	});
-    
 };
 
 initialise();
